098a0737275a9cfc3a247ad424c3faad31750d5f
julia1403 <julia.stebel@onet.eu>
2023-01-05 11:05:26 +0100

    - zarządanie panelem admina - tworzenie bazy w models

--- wsjsite/home/admin.py
+++ wsjsite/home/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Service, Employee, Comment, Client
 # Register your models here.
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("service_name",)
+    list_filter = ("service_name","price","approx_time",)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name",)
+    list_filter = ("first_name","last_name",)
+
+
+class CommentAdmin(admin.ModelAdmin):
+     list_display = ("user_name",)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name","phone_number",)
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Client)
--- wsjsite/home/models.py
+++ wsjsite/home/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Service(models.Model):
+    service_name = models.CharField(max_length= 100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    approx_time = models.DurationField()
+
+    def __str__(self):
+        return f"{self.service_name}"
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(validators=[MinLengthValidator(9)], unique=True, max_length=20)
+    email = models.EmailField(unique=True)
+    login = models.CharField(validators = [MinLengthValidator(5)], max_length=20, unique=True)
+    password = models.CharField(validators=[MinLengthValidator(8)], max_length=20)
+    image = models.ImageField(upload_to="photos", null=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(validators=[MinLengthValidator(9)], unique=True, max_length=20)
+    email = models.EmailField(unique=True)
+    password = models.CharField(validators=[MinLengthValidator(8)], max_length=20)
+    
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    service_name = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
+    worker_name = models.ForeignKey(Employee,on_delete=models.SET_NULL, null=True)
+    text = models.TextField(max_length=400)
