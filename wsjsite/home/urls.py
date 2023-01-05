cf4ae7814ea0c652167939cd651c83eeabafbb51
julia1403 <julia.stebel@onet.eu>
2023-01-05 18:37:59 +0100

    - początek tworzenia formularza rejestracji - szkielet (bez połączenia z bazą)

--- wsjsite/home/urls.py
+++ wsjsite/home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from home.views import signup 
+
 
 urlpatterns = [
-    path('', views.index, name='index')    
+    path('', views.index, name='index'),
+    path("signup/",signup, name="signup"),
+
 ]
--- wsjsite/home/views.py
+++ wsjsite/home/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime
-
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth.forms import UserCreationForm 
+ 
+from .forms import SignUpForm
 # Create your views here.
 
 TEMPLATE_DIRS = (
@@ -10,6 +13,19 @@ TEMPLATE_DIRS = (
 
 def index(request):
     today = datetime.datetime.now().date()
-    return render(request, "index.html", {"today": today})
+    return render(request, "home/index.html", {"today": today})
 
-
+# Sign Up View
+def signup(request): 
+    form = SignUpForm(request.POST) 
+    if form.is_valid(): 
+        form.save() 
+        username = form.cleaned_data.get('username') 
+        password = form.cleaned_data.get('password') 
+        user = authenticate(username=username, password=password) 
+        login(request, user) 
+        return redirect('home') 
+    context = { 
+        'form': form 
+    } 
+    return render(request, 'home/sign-up.html', context) 
