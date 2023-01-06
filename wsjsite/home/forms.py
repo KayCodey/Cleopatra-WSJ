cadae60378e9a748aaa4950546dc1c22808627b2
julia1403 <julia.stebel@onet.eu>
2023-01-06 12:44:01 +0100

    - formularz rejestracji

--- wsjsite/home/forms.py
+++ wsjsite/home/forms.py
@@ -1,10 +1,30 @@
 from django import forms 
-from django.contrib.auth.models import User 
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Client 
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = '__all__'
+        labels = {
+            "first_name": "Imię",
+            "last_name": "Nazwisko",
+            "phone_number": "Numer telefonu",
+            "email": "Adres e-mail",
+            "password": "Hasło"
+
+        }
  
-class SignUpForm(UserCreationForm): 
-    class Meta: 
-        model = User 
-        fields = ('username', 'email', 'password1', 'password2', ) 
-    
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields=['username','email','password1','password2']
+        labels = {
+            "username": "Login",
+            "email": "Adres e-mail",
+            "password1": "Hasło",
+            'password2': "Powtórz hasło"
+
+        }
+
--- wsjsite/home/views.py
+++ wsjsite/home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime
+from .forms import SignupForm, CreateUserForm
 from django.contrib.auth import login, authenticate 
 from django.contrib.auth.forms import UserCreationForm 
- 
-from .forms import SignUpForm
+from django.contrib import messages
 # Create your views here.
 
 TEMPLATE_DIRS = (
@@ -17,15 +17,23 @@ def index(request):
 
 # Sign Up View
 def signup(request): 
-    form = SignUpForm(request.POST) 
-    if form.is_valid(): 
-        form.save() 
-        username = form.cleaned_data.get('username') 
-        password = form.cleaned_data.get('password') 
-        user = authenticate(username=username, password=password) 
-        login(request, user) 
-        return redirect('home') 
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST) 
+        if form.is_valid(): 
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for '+ user)
+        #name = form.cleaned_data.get('first_name') 
+        #last_name  = form.cleaned_data.get('last_name')
+        #phone = form.cleaned_data.get('phone_number')
+        #email_address = form.cleaned_data.get('email')
+        #password = form.cleaned_data.get('password') 
+        #user = authenticate(name=name, last_name=last_name,phone = phone, email_address = email_address,password=password)
+        #user = form.save()
+        #login(request, user) 
+        #return redirect('home/index.html') 
     context = { 
-        'form': form 
+        'signup_form': form 
     } 
     return render(request, 'home/sign-up.html', context) 
